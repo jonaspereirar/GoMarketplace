97f175462c9f4d118c5374e0db44316048330146
jonaspereirar <jonaspereirar@gmail.com>
2020-05-04 20:43:40 +0100

    feat. add.: retorna valor total dentro do carrinho
    Signed-off-by: jonaspereirar <jonaspereirar@gmail.com>

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,19 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const sum = products.reduce((prevVal, elem) => {
+      return prevVal + elem.quantity * elem.price;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(sum);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const sum = products.reduce((prevVal, elem) => {
+      return prevVal + elem.quantity;
+    }, 0);
 
-    return 0;
+    return sum;
   }, [products]);
 
   return (
